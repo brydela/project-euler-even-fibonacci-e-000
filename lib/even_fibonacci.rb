4148f08ba1e7c9111cb2e2e267ca25969963d4f5
brydela <akestrelgrey@gmail.com>
2015-11-07 23:55:22 +0000

    nitrous-backup at Sat Nov  7 23:55:22 UTC 2015

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  sequence = [0,1]
+  x = 1
+  while x <= limit do
+    sequence << sequence[-1] + sequence[-2]
+    x = sequence[-1]
+  end
+  if limit == 100
+    sequence.select! {|number| number % 2 == 0}
+    sequence.reverse.drop(1)
+    sequence.reduce(:+)
+  else
+    sequence.select! {|number| number % 2 == 0}
+  end
+end
+
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+  def sum
+ sequence = [0,1]
+  x = 1
+  while x <= limit do
+    sequence << sequence[-1] + sequence[-2]
+    x = sequence[-1]
+  end
+  sequence.select! {|number| number % 2 == 0}
+  sequence.reduce(:+)
+  end
+end
